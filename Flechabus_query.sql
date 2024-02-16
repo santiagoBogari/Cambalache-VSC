c4348593f9de430d4e41bcd94cd26b6ff22ac236
santiagobogari <santiagobogari@gmail.com>
2024-02-16 16:04:55 -0300

    flecha

--- Flechabus_query.sql
+++ Flechabus_query.sql
@@ -183,3 +183,73 @@ FROM (
 INNER JOIN SFImport_Account
     ON Subquery.EmailAddress = SFImport_Account.PersonEmail
 WHERE Subquery.RowNumber = 1
+
+
+-----------------------------------------------------------------------------------------------------
+
+select
+AC.id,
+AC.PersonContactId,
+AC.Name as Nombre,
+AC.FirstName,
+AC.LastName,
+AC.Type as Tipo,
+AC.RecordTypeId,
+AC.Phone as Telefono,
+AC.OwnerID,
+AC.CreatedDate as Fecha_Creacion,
+AC.AccountSource as Origen_Cuenta,
+AC.Tipo_de_Documento__PC,
+AC.Nro_Documento__pc,
+AC.ID_Pasajero__PC,
+AC.Id_del_pasajero__c,
+AC.Fecha_de_Nacimiento__c,
+AC.PersonBirthDate,
+AC.Email__c,
+AC.PersonEmail,
+AC.Phone,
+AC.IsPersonAccount
+from SFImport_Account as AC
+where AC.PersonEmail is not null or
+Email__c is not null
+
+--(1474174 rows) 
+
+/*  */
+
+select
+AC.id,
+AC.PersonContactId,
+AC.Name as Nombre,
+AC.FirstName,
+AC.LastName,
+AC.Type as Tipo,
+AC.RecordTypeId,
+AC.Phone as Telefono,
+AC.OwnerID,
+AC.CreatedDate as Fecha_Creacion,
+AC.AccountSource as Origen_Cuenta,
+AC.Tipo_de_Documento__PC,
+AC.Nro_Documento__pc,
+AC.ID_Pasajero__PC,
+AC.Id_del_pasajero__c,
+AC.Fecha_de_Nacimiento__c,
+AC.PersonBirthDate,
+AC.Email__c,
+AC.PersonEmail,
+AC.Phone,
+AC.IsPersonAccount
+
+from SFImport_Account as AC
+left join SFImport_Boletos as BO on BO.Id_Pasajero_Salesforce__c = AC.Id
+where 
+AC.PersonEmail is not null and
+AC.IsPersonAccount = '1' and
+BO.Empresa_Venta__c = 'Chevallier' or 
+BO.Empresa_Venta__c = 'Flechabus' or
+BO.Empresa_Venta__c = 'Urquiza' or
+BO.Empresa_Venta__c = 'La Veloz'
+
+-- (1016430 rows)
+
+/*  */
