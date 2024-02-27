12d35681ddbb091ee742f7c021a15f09493a5fdc
santiagobogari <santiagobogari@gmail.com>
2024-02-27 18:33:01 -0300

    query vital

--- Vital/Bounce.sql
+++ Vital/Bounce.sql
@@ -1,5 +1,5 @@
 /* 27-2-2024 BOUNCE (condiciones de exclusión) quitar todos los Bounce !=  SOFT BOUNCE = mailbox full , EventDate > 15 dias
-  Data Extensions / Sendlog_and_DataViews / Bounce */
+  Data Extensions / Sendlog_and_DataViews / Bounce     72.337 records   */
 
 SELECT 
   BounceTypeID,
@@ -85,3 +85,146 @@ WHERE BounceSubcategory != 'Mailbox Full' AND DATEDIFF(day, EventDate, GETDATE()
 
 --  (35138 rows)
 
+/*  */
+
+SELECT 
+  BounceTypeID,
+  TriggererSendDefinitionObjectID,
+  IsUnique,
+  BounceSubcategory,
+  TriggeredSendCustomerKey,
+  BounceCategory,
+  SubscriberID,
+  BounceType,
+  Domain,
+  IsFalseBounce,
+  AccountID,
+  OYBAccountID,
+  BounceCategoryID,
+  EventDate,
+  ListID,
+  BounceSubcategoryID,
+  BatchID,
+  SMTPCode,
+  SubscriberKey,
+  JobID
+FROM
+  Bounce
+WHERE BounceSubcategory != 'Mailbox Full' AND EventDate > 15
+
+-- (35768 rows)
+
+/*  */
+SELECT 
+  BounceTypeID,
+  TriggererSendDefinitionObjectID,
+  IsUnique,
+  BounceSubcategory,
+  TriggeredSendCustomerKey,
+  BounceCategory,
+  SubscriberID,
+  BounceType,
+  Domain,
+  IsFalseBounce,
+  AccountID,
+  OYBAccountID,
+  BounceCategoryID,
+  EventDate,
+  ListID,
+  BounceSubcategoryID,
+  BatchID,
+  SMTPCode,
+  SubscriberKey,
+  JobID
+FROM
+  BOUNCE
+WHERE
+  NOT (BounceSubcategory = 'Mailbox Full' AND DATEDIFF(day, DATEADD(day, 15, EventDate), GETDATE()) >= 0)
+
+  -- (42316 rows)
+
+  /*  */
+
+  SELECT 
+  BounceTypeID,
+  TriggererSendDefinitionObjectID,
+  IsUnique,
+  BounceSubcategory,
+  TriggeredSendCustomerKey,
+  BounceCategory,
+  SubscriberID,
+  BounceType,
+  Domain,
+  IsFalseBounce,
+  AccountID,
+  OYBAccountID,
+  BounceCategoryID,
+  EventDate,
+  ListID,
+  BounceSubcategoryID,
+  BatchID,
+  SMTPCode,
+  SubscriberKey,
+  JobID
+FROM
+  QueryStudioResultsat20240227T1743000000
+WHERE
+  BounceSubcategory = 'Mailbox Full' 
+
+  --  (6548 rows)
+
+SELECT 
+  BounceTypeID,
+  TriggererSendDefinitionObjectID,
+  IsUnique,
+  BounceSubcategory,
+  TriggeredSendCustomerKey,
+  BounceCategory,
+  SubscriberID,
+  BounceType,
+  Domain,
+  IsFalseBounce,
+  AccountID,
+  OYBAccountID,
+  BounceCategoryID,
+  EventDate,
+  ListID,
+  BounceSubcategoryID,
+  BatchID,
+  SMTPCode,
+  SubscriberKey,
+  JobID
+FROM
+  BOUNCE
+WHERE
+  NOT (BounceSubcategory = 'Mailbox Full' AND (EventDate + 15) < GETDATE())
+  -- (42316 rows)
+
+  /*  */
+  SELECT 
+  BounceTypeID,
+  TriggererSendDefinitionObjectID,
+  IsUnique,
+  BounceSubcategory,
+  TriggeredSendCustomerKey,
+  BounceCategory,
+  SubscriberID,
+  BounceType,
+  Domain,
+  IsFalseBounce,
+  AccountID,
+  OYBAccountID,
+  BounceCategoryID,
+  EventDate,
+  ListID,
+  BounceSubcategoryID,
+  BatchID,
+  SMTPCode,
+  SubscriberKey,
+  JobID
+FROM
+  BOUNCE
+WHERE
+  (BounceSubcategory = 'Mailbox Full' AND (EventDate + 15) < GETDATE())
+
+  -- (30021 rows)
