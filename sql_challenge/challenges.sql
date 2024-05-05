07891afc1d22c595990d31f0366eed78bf4dfcff
santiagobogari <santiagobogari@gmail.com>
2024-05-05 17:11:33 -0300

    sql challenge

--- sql_challenge/challenges.sql
+++ sql_challenge/challenges.sql
@@ -161,3 +161,124 @@ FROM   NextMobile_Users as US
 GROUP BY US.state
 
 --day 22
+
+SELECT 
+    DISTINCT u.id, 
+    u.first_name, 
+    u.last_name, 
+    u.email
+FROM [NextMobile_Users] u
+JOIN [NextMobile_Orders] o ON u.id = o.customer_id
+WHERE o.date BETWEEN DATEADD(day, -60, GETDATE()) AND DATEADD(day, -15, GETDATE())
+AND o.status = 'shipped'
+
+--day 23
+
+SELECT
+SRV.id, SRV.last_name, Resp.nps_score
+FROM NextMobile_Survey as SRV
+LEFT JOIN NextMobile_Survey_Responses as Resp on SRV.id = Resp.id
+
+--day 24
+
+SELECT
+    id,
+    nps_score
+FROM   [NextMobile_Survey_Responses]
+UNION
+SELECT
+    id,
+    nps_score
+FROM   [NextMobile_Survey_Responses_Sales]
+
+--day 25
+
+SELECT 
+US.id, US.last_name
+FROM NextMobile_Users as US
+
+WHERE US.id IN (SELECT customer_id
+    FROM NextMobile_Orders_2023
+    GROUP BY customer_id
+    HAVING SUM(amount) > 10000)
+
+--day 26
+
+SELECT 
+ o.state, COUNT(o.order_id) AS order_count
+
+FROM (
+        SELECT o.order_id, u.state
+        FROM NextMobile_Orders_2023 as o
+        JOIN NextMobile_Users as u
+        on o.customer_id = u.id
+        WHERE o.status = 'shipped'
+)AS o
+
+GROUP BY o.state
+
+--day 27
+
+SELECT 
+    id,
+    email,
+    o.eventdate AS opened,
+    c.eventdate AS clicked
+FROM [NextMobile_Users] u
+LEFT JOIN   [NextMobile_Open] o
+ON   o.subscriberkey = u.id
+LEFT JOIN   [NextMobile_Click] c
+ON  c.jobid = o.jobid AND c.subscriberkey = o.subscriberkey
+WHERE
+  o.jobid = '765452' OR o.jobid IS NULL
+
+--day 28
+
+SELECT
+    COUNT(subscriberkey),
+    domain,
+    LEFT(SMTPBounceReason,100) AS BounceReason
+FROM
+    [NextMobile_Bounce]
+WHERE
+    eventdate BETWEEN '2024-02-01' AND '2024-02-29'
+GROUP BY
+    domain,
+    LEFT(SMTPBounceReason,100)
+HAVING
+    COUNT(subscriberkey) > 1
+
+--day 29
+
+SELECT
+    DATENAME(WEEKDAY, eventdate) AS day_of_week,
+    COUNT(subscriberkey) AS unsubscribe_count
+FROM
+    NextMobile_Unsubscribe
+GROUP BY
+    DATENAME(WEEKDAY, eventdate)
+
+--day 30
+
+SELECT
+    u.state,
+    (SUM(CASE WHEN c.IsUnique = 1 THEN 1.0 ELSE 0 END) / (COUNT(s.SubscriberKey) - SUM(CASE WHEN b.IsUnique = 1 THEN 1.0 ELSE 0 END))) * 100 AS ctr
+FROM
+    [NextMobile_Users] u
+JOIN 
+    [NextMobile_Sent_2023] s 
+ON
+    u.id = s.SubscriberKey
+LEFT JOIN
+    [NextMobile Click_2023] c
+ON
+    s.SubscriberKey = c.SubscriberKey AND s.JobId = c.JobId
+LEFT JOIN
+    [NextMobile_Bounce_2023] b
+ON
+    s.SubscriberKey = b.SubscriberKey AND s.JobId = b.JobId    
+WHERE
+    s.EventDate >= '2023-01-01' 
+    AND s.EventDate <= '2023-12-31' 
+GROUP BY
+    u.state
