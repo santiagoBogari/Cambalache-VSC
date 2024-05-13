7a86156eda3addda7be4e79447cfbd6b12db55d8
santiagobogari <santiagobogari@gmail.com>
2024-05-13 19:46:43 -0300

    queries

--- Vital/co_acc_emp.sql
+++ Vital/co_acc_emp.sql
@@ -44,3 +44,45 @@ u1bm.Email IS NULL AND
 ubh.EmailAddress IS NULL AND CO.Email IS NOT NULL
 
 /* (28217 rows) */
+/* (25690 rows) con CO.Email IS NOT NULL*/
+/* (25719 rows) sin CO.Email IS NOT NULL*/
+SELECT
+CO.Id as Contact_ID,
+CO.Email as Contact_Email,
+CO.Name as Contact_Name,
+EMP.Nombre as Account_Name
+FROM SFIMport_Contacts as CO
+INNER JOIN Empresas as EMP on CO.AccountId = EMP.Id
+
+LEFT JOIN Unsubscribed_Bounce_Held AS ubh ON ubh.EmailAddress = CO.Email
+LEFT JOIN Unsubscribed_1895_1_BBDD_Maestra AS u1bm ON u1bm.Email = CO.Email
+WHERE 
+u1bm.Email IS NULL AND
+ubh.EmailAddress IS NULL
+/* 
+ */
+SELECT
+CO.Id as Contact_ID,
+CO.Email as Contact_Email,
+CO.Name as Contact_Name,
+EMP.Nombre as Account_Name,
+EMP.Sucursal as Sucursal,
+EMP.Rubro1 as Empresa_Rubro,
+Acc.Industry as Account_Industry
+FROM SFIMport_Contacts as CO
+LEFT JOIN SFIMport_Accounts as Acc on Acc.Id = CO.AccountId
+INNER JOIN Empresas as EMP on CO.AccountId = EMP.Id
+
+LEFT JOIN Unsubscribed_Bounce_Held AS ubh ON ubh.EmailAddress = CO.Email
+LEFT JOIN Unsubscribed_1895_1_BBDD_Maestra AS u1bm ON u1bm.Email = CO.Email
+WHERE 
+u1bm.Email IS NULL AND
+ubh.EmailAddress IS NULL
+/* (25719 rows) */
+
+/*  */
+SELECT Account_Industry 
+FROM [Empresas_Contacts_Emails] 
+WHERE Account_Industry IS NOT NULL and Account_Industry != 'Otro' and Account_Industry != ''
+GROUP BY Account_Industry
+/*  */
